91e88c9c9070f6ed067c9042d31845f0d1b15a15
RickFAF <rickfaf1@gmail.com>
2019-05-28 19:03:22 +0100

    work on cli 2

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import commandLineArgs = require("command-line-args")
 import commandLineUsage = require("command-line-usage")
+import { existsSync, readFileSync, writeFileSync } from "fs"
+import { basename, dirname, extname } from "path"
+import { TypeOnlyAst } from "./ast"
+import { parseTypeOnlyToAst } from "./parser/parse-typeonly"
 
 class InvalidArgumentError extends Error {
   readonly causeCode = "invalidArgument"
@@ -53,14 +57,17 @@ function cli() {
   const options = parseOptions()
   if (!options)
     return
-  // console.log(options)
+  console.log(options["src"].toString())
+  // if (options.) {
+  //   console.log(Object.keys(options))
+  // }
   if (options["help"]) {
     printHelp()
     return
   }
 
   try {
-    proccessFile(options.src.file, options)
+    proccessFile(options["src"].toString(), options)
   } catch (error) {
     if (error.causeCode === "invalidArgument") {
       console.error(`Error: ${error.message}`)
@@ -108,4 +115,48 @@ function parseOptions(): object | undefined {
 function proccessFile(file: string, options) {
   if (!options.src)
     throw new InvalidArgumentError("Missing Source File")
+
+  let input: string
+  try {
+    input = readFileSync(file, { encoding: options.encoding || "utf8" }) as any
+  } catch (err) {
+    throw new InvalidArgumentError(`Cannot read file: ${file}`)
+  }
+  const bnad = baseNameAndDir(file)
+  const ast: TypeOnlyAst = parseTypeOnlyToAst(input)
+  createAstJsonFile(ast, options, bnad)
+}
+
+function createAstJsonFile(ast: TypeOnlyAst, options, bnad: BaseNameAndDir) {
+  const generateAst = JSON.stringify(ast, undefined, 2)
+  const dir = normalizePath(options["output-dir"], bnad.directory)
+  const outFile = `${dir}/${bnad.fileBaseName}.ast.json`
+  if (!options.force && existsSync(outFile))
+    throw new Error(`Cannot overwrite existing file: ${outFile}`)
+  writeFileSync(outFile, generateAst, {
+    encoding: options.encoding || "utf8",
+  })
+}
+
+function normalizePath(path: string | undefined, defaultPath?: string): string | undefined {
+  if (!path)
+    return defaultPath
+  path = path.replace(/\/+$/, "")
+  if (path)
+    return path
+}
+
+interface BaseNameAndDir {
+  directory: string
+  fileBaseName: string
+  extension: string
+}
+
+function baseNameAndDir(file: string): BaseNameAndDir {
+  const extension = extname(file)
+  return {
+    directory: dirname(file),
+    fileBaseName: basename(file, extension),
+    extension
+  }
 }
