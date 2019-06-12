66649f4b10e099c56656e9128e08185053c4176f
Paleo <paleo@paleo.casa>
2019-06-12 09:31:26 +0100

    Improve error messages

--- src/error-message.ts
+++ src/error-message.ts
@@ -21,12 +21,10 @@ export function makeErrorMessage(unmatchs: Unmatch[]): string {
 function valueAsString(val: unknown): string {
   switch (typeof val) {
     case "string":
-      return `'${JSON.stringify(val.length <= 12 ? val : val.substr(0, 12) + "…")}'`
     case "number":
     case "boolean":
-      return `'${val}'`
     case "bigint":
-      return `'${val}n'`
+      return `'${primitiveValueAsString(val)}n'`
     case "object":
       if (val === null)
         return "null"
@@ -36,17 +34,29 @@ function valueAsString(val: unknown): string {
     case "undefined":
       return typeof val
     default:
-      throw new Error(`Unexpected typeof: ${typeof val}`)
+      throw new Error(`Unexpected typeof val: ${typeof val}`)
+  }
+}
+
+function primitiveValueAsString(val: string | number | bigint | boolean) {
+  switch (typeof val) {
+    case "string":
+      return JSON.stringify(val.length <= 12 ? val : val.substr(0, 12) + "…")
+    case "number":
+    case "boolean":
+      return String(val)
+    case "bigint":
+      return `${val}n`
+    default:
+      throw new Error(`Unexpected primitive type: ${typeof val}`)
   }
 }
 
 function objectAsString(obj: object) {
   return `{${Object.entries(obj).map(([key, val]) => {
     const t = typeof val
-    if (t === "string")
-      return `${key}: ${JSON.stringify(val)}`
-    if (t === "number" || t === "bigint" || t === "boolean")
-      return `${key}: ${val}`
+    if (t === "string" || t === "number" || t === "bigint" || t === "boolean")
+      return `${key}: ${primitiveValueAsString(val)}`
     return key
   }).join(", ")}}`
 }
